74a1b87c461bba91cb71a9715cf9594855c6cbe1
Angie Hinrichs <angie@soe.ucsc.edu>
2025-08-28 10:07:29 -0700

    Libify useful bits into viral_usher_trees.py.  Create job-partitioning script for new workflow.

--- scripts/generate_configs.py
+++ scripts/generate_configs.py
@@ -6,33 +6,11 @@ Make tree subdirectories and config files (if they don't already exist) for viru
 import argparse
 import csv
 import os
-import subprocess
 import sys
 
+import viral_usher_trees
+
 metadata_required_columns = ["accession", "tree_name", "assembly", "Taxonomy_ID"]
-config_name = "config.toml"
-
-
-def generate_config(subdir_path: str, tree_name: str, refseq_acc: str, refseq_assembly: str, taxid: str) -> bool:
-    """Make subdir_path/config.toml using viral_usher init with command-line args
-    to skip the interactive process."""
-    command = ["viral_usher", "init",
-               "--refseq", refseq_acc,
-               "--taxonomy_id", taxid,
-               # Nextclade dataset search is not working well enough, and command fails if there are multiple matches,
-               # so just say no to nextclade by default; add back manually to config.toml files where applicable.
-               "--nextclade_dataset", "",
-               "--workdir", subdir_path,
-               "--config", subdir_path + "/" + config_name]
-    print(" ".join(command))
-    try:
-        subprocess.run(command, check=True)
-        return True
-    except FileNotFoundError as e:
-        print(f"viral_usher needs to be in your path. ({e})", file=sys.stderr)
-        sys.exit(1)
-    except subprocess.CalledProcessError:
-        return False
 
 
 def generate_configs(input_tsv: str):
@@ -54,12 +32,12 @@ def generate_configs(input_tsv: str):
             tree = row["tree_name"]
             asm = row["assembly"]
             taxid = row["Taxonomy_ID"]
-            subdir_path = "trees/" + tree
+            subdir_path = viral_usher_trees.tree_dir + "/" + tree
             if not os.path.isdir(subdir_path):
                 os.mkdir(subdir_path)
-            config_path = subdir_path + "/" + config_name
+            config_path = subdir_path + "/" + viral_usher_trees.config_name
             if not os.path.exists(config_path):
-                if not generate_config(subdir_path, tree, acc, asm, taxid):
+                if not viral_usher_trees.generate_config(subdir_path, tree, acc, asm, taxid):
                     failures.append(tree)
         if failures:
             print("\n*** Some viral_usher init commands failed:", file=sys.stderr)
@@ -73,9 +51,7 @@ def main():
     parser.add_argument('-i', '--input', required=True,
                         help="Input RefSeq tree metadata TSV file, must include columns " + ", ".join(metadata_required_columns))
     args = parser.parse_args()
-    if not os.path.isdir("trees"):
-        print("Can't find trees directory.  This script must be run in top level of repo.", file=sys.stderr)
-        sys.exit(1)
+    viral_usher_trees.check_top_level_dir()
     generate_configs(args.input)
 
 
