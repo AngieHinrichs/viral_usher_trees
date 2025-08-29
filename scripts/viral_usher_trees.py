cd0038ed92c98a2828a296e0df896297047a3062
Angie Hinrichs <angie@soe.ucsc.edu>
2025-08-28 21:12:20 -0700

    When generating config file, replace workdir's absolute path with .-relative. Otherwise the jobs will fail due to wrong absolute path.

--- scripts/viral_usher_trees.py
+++ scripts/viral_usher_trees.py
@@ -21,6 +21,8 @@ def check_top_level_dir():
 def generate_config(subdir_path: str, tree_name: str, refseq_acc: str, refseq_assembly: str, taxid: str) -> bool:
     """Make subdir_path/config.toml using viral_usher init with command-line args
     to skip the interactive process."""
+    config_path = subdir_path + "/" + config_name
+    config_path_tmp = config_path + ".tmp"
     command = ["viral_usher", "init",
                "--refseq", refseq_acc,
                "--taxonomy_id", taxid,
@@ -28,10 +30,19 @@ def generate_config(subdir_path: str, tree_name: str, refseq_acc: str, refseq_as
                # so just say no to nextclade by default; add back manually to config.toml files where applicable.
                "--nextclade_dataset", "",
                "--workdir", subdir_path,
-               "--config", subdir_path + "/" + config_name]
+               "--config", config_path_tmp]
     print(" ".join(command))
     try:
         subprocess.run(command, check=True)
+        # Replace the absolute path for workdir
+        with open(config_path_tmp, "r") as cfg_in, open(config_path, "w") as cfg_out:
+            for line in cfg_in:
+                if line.startswith("workdir"):
+                    ix = line.index("/" + trees_dir + "/")
+                    cfg_out.write("workdir = '." + line[ix:])
+                else:
+                    cfg_out.write(line)
+        os.remove(config_path_tmp)
         return True
     except FileNotFoundError as e:
         print(f"viral_usher needs to be in your path. ({e})", file=sys.stderr)
